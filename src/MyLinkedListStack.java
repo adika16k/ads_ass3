5171ff819f6b14e6e99ebcaede6f51f140916433
adika16k <adilhankenzaliev@gmail.com>
2023-04-30 16:04:51 +0600

    ass3

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,4 +1,9 @@
 import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
+    private MyLinkedList<T> list;
+
+    public MyLinkedListStack(){
+        list = new MyLinkedList<>();
+    }
 
 }
