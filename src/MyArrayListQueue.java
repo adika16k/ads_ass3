60d93cd8e6b9efe59b704f476007cb422fb19314
adika16k <adilhankenzaliev@gmail.com>
2023-05-01 17:30:25 +0600

    ass3

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -6,22 +6,22 @@ public class MyArrayListQueue<T> {
         list = new MyArrayList<>();
     }
     public void enqueue(T element){
-        list.add(element); // add the element to the end of the queue.
+        list.add(element); // add the element to the end of the queue
     }
     public boolean isEmpty() {
-        return list.getSize() == 0; // return a boolean whether the queue is empty or not.
+        return list.getSize() == 0; // return a boolean whether the queue is empty or not
     }
     public T dequeue() {
         if (isEmpty()) {
-            throw new NoSuchElementException("Queue is empty");
+            throw new NoSuchElementException("Queue is empty"); // Throws a NoSuchElementException if the queue is empty
         }
-        return list.remove(0);
+        return list.remove(0); //  removes and returns the element at the front of the queue
     }
     public T peek() {
         if (isEmpty()) {
-            throw new NoSuchElementException("Queue is empty");
+            throw new NoSuchElementException("Queue is empty"); // Throws a NoSuchElementException if the queue is empty
         }
-        return list.get(0);
+        return list.get(0); // return the element at the front of the queue without removing it
     }
     public int size() {
         return list.getSize();
